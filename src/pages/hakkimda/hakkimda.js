e3774eca65fa10a2697620380fde58efa4eee6a8
aybertocarlos <aybertocarlos@gmail.com>
2018-06-09 19:04:35 +0300

    hakkimda page first update

--- src/pages/hakkimda/hakkimda.js
+++ src/pages/hakkimda/hakkimda.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import {Col, Row, Container} from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+import {isMobile} from 'react-device-detect'
+
+import classes from './hakkimda.css'
+import BongSoon from '../../img/bongsoon.png'
 
 const hakkimda = () => {
+    const hyazi1 = "Eskiden PuzzleSubs Anime & Manga çeviri grubunda yöneticilik yapmış, ancak şu anda hiçbir grubun üyesi olmayan, gönüllü çevirmen bir birey.\n" +
+        "Son zamanlarda anime izlemeyi bırakıp, Kore dizilerinin dünyasına dalmıştır. Şu anki öncelikli amacı, Netflix üzerindeki Kore dizilerinin hepsini bitirmektir.\n"
+
+    const hyazi2 = "Dram & Komedi & Romantizm \"genre\"larının hayranı.\n" +
+        "Ne kadar cheesy o kadar güzel."
+
     return (
-        <div>hakkimda placeholder</div>
+        <React.Fragment>
+            <Container  className={classes.container}>
+                <Row>
+                    <Col md={{size: 9, offset: 3}} className={classes.header}>
+                        <span>Hakkımda</span>
+                    </Col>
+                    <Col md='12'>
+                        <Row>
+                            <Col md='3' className={isMobile ? classes["avatar-mobile"] : classes.avatar + ' ' + classes["background-shadow"]}>
+                                <img src={BongSoon} width="100%"/>
+                            </Col>
+                            <Col md='9' className={classes.yazi}>
+                                <p>{hyazi1}</p>
+                                <p>{hyazi2}</p>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </React.Fragment>
     )
 };
 
